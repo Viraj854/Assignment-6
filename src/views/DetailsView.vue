9c4194acc3974ef0bbb8aae2e6e3be59568f6f79
Viraj <oogoldenpigslayeroo@gmail.com>
2024-12-15 22:43:32 -0500

    Added back button to details view

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -6,8 +6,28 @@ import Details from '../components/Details.vue';
 
 <template>
     <Header />
+    <RouterLink to="/movies" class="back-button">Back</RouterLink>
     <Details />
     <Footer />
 </template>
 
-<style scoped></style>
+<style scoped>
+.back-button {
+    display: inline-block;
+    background-color: #4073ad;
+    color: #ffffff;
+    padding: 10px 30px;
+    font-size: 18px;
+    text-align: center;
+    text-decoration: none;
+    border: none;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+.back-button:hover {
+    background-color: #305a8a;
+}
+</style>
